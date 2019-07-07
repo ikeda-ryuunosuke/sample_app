7d05de2264710ed11cb30552e5027032fa20eb4e
EC2 Default User <ec2-user@ip-172-31-32-13.us-east-2.compute.internal>
2019-07-07 20:01:19 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-end
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  def hello
-    render html: "hello, world!"
-  end
+  include SessionsHelper
 end
