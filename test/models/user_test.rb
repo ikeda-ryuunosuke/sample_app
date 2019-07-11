3b216a049fcbf3934ea6d9330a7b53acdcfb4deb
EC2 Default User <ec2-user@ip-172-31-32-13.us-east-2.compute.internal>
2019-07-11 07:40:40 +0000

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -80,4 +80,12 @@ class UserTest < ActiveSupport::TestCase
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
